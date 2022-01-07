190ea51d0594087bc7d377470200f7d16784628a
unknown <rodriguezmateo583@gmail.com>
2022-01-07 19:33:56 -0300

    res js1

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -186,9 +186,9 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
     //Si el número es 0, devuelve false
-    if (numero > 0) { return "Es Positivo" }
-    else if (numero < -1) { return "Es negativo" }
-    else { return false }
+    if (numero > 0) { return "Es positivo" }
+    if (numero < 0) { return "Es negativo" }
+    else  { return false }
 }
 
 function agregarSimboloExclamacion(str) {
@@ -209,7 +209,7 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
     // Tu código:
-    return "Hola${nombre}!";
+    return "Hola$" + {nombre} + "!";
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
